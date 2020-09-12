6e5abf9edc0396a751d2e648c568e2c6b1e213d7
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-12 14:10:27 +0000

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+let conut =document.getElementById("counter");
+function counter()
+ {
+  let i = 0;
+  // This block will be executed 100 times.
+  setInterval(function() {console.log(i++)}, 1000);
+
+}
+count.innerhtml
