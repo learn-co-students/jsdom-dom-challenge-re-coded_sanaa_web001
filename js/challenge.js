8a4d7886f4aa190e0f1233ffe78867f171f43b6f
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-25 12:22:24 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,64 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
+  const counter = document.getElementById('counter')
+  const plus = document.getElementById('+')
+  const minus = document.getElementById('-')
+  const pause = document.getElementById('pause')
+  const like = document.getElementById('<3')
+  const likes = document.getElementsByClassName('likes')[0]
+  const submit = document.getElementById('submit')
+  const commentForm = document.getElementsByTagName('input')[0]
+  const comments = document.getElementById('list')
+  let speedCounter = setInterval(plusCounter, 1000)
+  let gameRunning = true
 
-})
+    function plusCounter() {
+      if (gameRunning === true){
+       counter.innerHTML++
+     }
+    };
 
-let conut =document.getElementById("counter");
-function counter()
- {
-  let i = 0;
-  // This block will be executed 100 times.
-  setInterval(function() {console.log(i++)}, 1000);
+    plus.addEventListener("click", function() {
+      counter.innerHTML++
+    });
 
-}
-count.innerhtml
+    minus.addEventListener("click", function() {
+      counter.innerHTML--
+    });
+
+    like.addEventListener("click", function() {
+      const likesList = document.createElement("LI");
+      let likeTracker = document.createTextNode(`${counter.innerHTML} has been liked 1 times.`)
+      likesList.appendChild(likeTracker);
+      likes.appendChild(likesList);
+    });
+
+    submit.addEventListener("click", function() {
+      event.preventDefault();
+      const newComment = document.createElement("p");
+      let commentInfo = document.createTextNode(`${commentForm.value}`);
+      newComment.appendChild(commentInfo);
+      comments.appendChild(newComment);
+      commentForm.value ='';
+    });
+
+
+  pause.addEventListener("click", function() {
+   if (gameRunning === true){
+     gameRunning = false
+     pause.innerHTML = "resume";
+     like.disabled = true;
+     minus.disabled = true;
+     plus.disabled = true;
+     submit.disabled = true;
+     commentForm.disabled = true;
+   }
+   else {
+     gameRunning = true;
+     pause.innerHTML = "pause"
+     like.disabled = false;
+     minus.disabled = false;
+     plus.disabled = false;
+     submit.disabled = false;
+     commentForm.disabled = false;
+   };
+  });
